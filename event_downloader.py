77f26db5c1041ff6f5f60efbfbfb3784e4392b85
jpbreuer <jeanpaul.breuer@gmail.com>
2016-07-13 23:25:59 -0400

    fixed weird appending bug

--- event_downloader.py
+++ event_downloader.py
@@ -12,7 +12,7 @@ import requests
 from datetime import date, timedelta as td
 
 d1 = date(2012, 9, 30)
-d2 = date(2014, 4, 2)
+d2 = date(2014, 10, 2)
 
 delta = d2 - d1
 
@@ -20,29 +20,41 @@ datelist = []
 for i in range(delta.days + 1):
     datelist.append(d1 + td(days=i))
 
-data_array = ""
+startindex = 0,93,365+93
+stopindex = 92,365+92,len(datelist)-1
 
-for ii in range(len(datelist)-1):
-#    link = 'http://www.nmdb.eu/nest/draw_graph.php?formchk=1&stations[]=KIEL' + \
-    link = 'http://www.nmdb.eu/nest/draw_graph.php?formchk=1&allstations=1' + \
-    '&output=ascii&force=1&tabchoice=revori&dtype=corr_for_efficiency&' + \
-    'tresolution=5&date_choice=bydate&' + \
-    'start_year=' + datelist[ii].strftime('%Y') + '&start_month=' + datelist[ii].strftime('%m') + '&start_day=' + datelist[ii].strftime('%d') + '&start_hour=' + '00' + '&start_min=' + '00' +'&' + \
-    'end_year=' + datelist[ii+1].strftime('%Y') + '&end_month=' + datelist[ii+1].strftime('%m') + '&end_day=' + datelist[ii+1].strftime('%d') + '&end_hour=' + '23' + '&end_min=' + '59'  #right now start_time is 2012-09-30 00:00 #right now end_time is 2013-09-30 23:59
+STATIONS = 'AATB','APTY','ARNM','ATHN','BKSN','BURE','CALG','CALM','DOMB','DOMC','DRBS','ESOI','FSMT','INVK','IRK2','IRK3','IRKT','JUNG','JUNG1','KERG','KGSN','KIEL','LMKS','MCMU','MCRL','MGDN','MOSC','MRNY','MWSN','MXCO','NAIN','NANM','NEWK','NRLK','NVBK','OULU','POTC','PTFM','PWNK','ROME','SANA','SOPB','SOPO','TERA','THUL','TSMB','TXBY','YKTK','KIEL2','NEU3','TIBT'
 
-    page = requests.get(link)
-    tree = html.fromstring(page.content)
+#STATIONS = 'THUL','YKTK'
 
-    header = tree.xpath('//code')[0].text[0:725]
-    stations = tree.xpath('//code')[0].text[725:957]
-    data = tree.xpath('//code')[0].text[957:]
+for jj in range(len(STATIONS)+1):
+    data_array = ""
+    for ii in range(len(startindex)):
+        #    link = 'http://www.nmdb.eu/nest/draw_graph.php?formchk=1&stations[]=KIEL' + \
+        link = 'http://www.nmdb.eu/nest/draw_graph.php?formchk=1&stations[]=' + STATIONS[jj] + \
+        '&output=ascii&force=1&tabchoice=revori&dtype=corr_for_efficiency&' + \
+        'tresolution=5&date_choice=bydate&' + \
+        'start_year=' + datelist[startindex[ii]].strftime('%Y') + '&start_month=' + datelist[startindex[ii]].strftime('%m') + '&start_day=' + datelist[startindex[ii]].strftime('%d') + '&start_hour=' + '00' + '&start_min=' + '00' +'&' + \
+        'end_year=' + datelist[stopindex[ii]].strftime('%Y') + '&end_month=' + datelist[stopindex[ii]].strftime('%m') + '&end_day=' + datelist[stopindex[ii]].strftime('%d') + '&end_hour=' + '23' + '&end_min=' + '59'  #right now start_time is 2012-09-30 00:00 #right now end_time is 2013-09-30 23:59
+        
+        page = requests.get(link)
+        tree = html.fromstring(page.content)
+        
+        if (tree.xpath('/html/body/font/b') == []):
+            full = tree.xpath('//code')[0].text#[0:725]
+            stations = tree.xpath('//code')[0].text[745:750]
+            data = tree.xpath('//code')[0].text[778:]
+            
+            data_array += data
+        else:
+            continue #elif (tree.xpath('/html/body/font/b')[0].text == 'Sorry, no data available'):
+        print('Iteration', str(ii+1), 'of', len(startindex), 'completed!')
     
-    data_array += data
-    print('Iteration', str(ii), 'of', len(datelist), 'completed!')
-
+    f = open('NMDB_' + STATIONS[jj] + '_data.txt', 'w')
+    f.write('Timestamp;    ')
+    f.write(stations)
+    f.write(data_array)
+    f.close()
+    print('Station', str(jj+1), 'of', len(STATIONS), 'finished....')
 #    data_array =  '\n'.join([data,string2,string3])
     
-f = open('NMDB_station_data.txt', 'w')
-f.write(stations)
-f.write(data_array)
-f.close()
